e709095157ff0c99e24e22ab2b2f257e17d0a566
footroot <footroot.72@gmail.com>
2024-05-28 19:51:29 +0100

    deploy try 3

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://astrolearn.netlify.app"
+    site: "https://astroprojects.netlify.app"
     // Add the site property to the Astro config with your site’s own unique Netlify URL.
 });
