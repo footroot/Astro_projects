a390e51256b205efed539601e44a1e0ee1c5783b
footroot <footroot.72@gmail.com>
2024-05-28 19:10:47 +0100

    commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://west-cottage.co.uk"
+    site: "https://astrolearn.netlify.app"
     // Add the site property to the Astro config with your site’s own unique Netlify URL.
 });
