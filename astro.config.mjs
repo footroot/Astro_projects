f8eeee21efd122f7ece03ccf825a7551e47ec158
footroot <footroot.72@gmail.com>
2024-05-28 18:45:31 +0100

    adding rss to the blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: "https://west-cottage.co.uk"
+    // Add the site property to the Astro config with your site’s own unique Netlify URL.
+});
